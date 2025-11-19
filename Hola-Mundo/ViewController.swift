a3c5432c063010c5fc90c316b4753a2547c5638a
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2025-11-19 09:40:50 -0600

    add button for increase

--- Hola-Mundo/ViewController.swift
+++ Hola-Mundo/ViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var lblCounter: UILabel!
+    var counter: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //view.backgroundColor = .red
         // Do any additional setup after loading the view.
+    }
+    @IBAction func increaseAction(_ sender: Any) {
+        counter += 1
+        print("Hola \(counter)")
+        lblCounter.text = "\(counter)"
     }
 }
 
